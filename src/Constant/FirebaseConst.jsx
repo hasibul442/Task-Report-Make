72ef9168d75af1639ff8587f9567d93a82325daf
hasibul-00026 <hasibul.hasan@bd.adventurekk.com>
2023-10-25 11:32:23 +0600

    Add Constent File for Firebase

---
+++ src/Constant/FirebaseConst.jsx
@@ -0,0 +1,7 @@
+export const FIREBASE_API_KEY = 'AIzaSyBOt-4cicZuFilwoSyI-YFiv_V2DMeB7U8';
+export const FIREBASE_AUTH_DOMAIN="task-maker-system.firebaseapp.com";
+export const FIREBASE_PROJECT_ID="task-maker-system";
+export const FIREBASE_STORAGE_BUCKET="task-maker-system.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID="703417551796";
+export const FIREBASE_APP_ID="1:703417551796:web:fbe015bbde4283eb3300e1";
+export const FIREBASE_MEASUREMENT_ID="G-DTSXX4LDJT";
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from "./Constant/FirebaseConst";
 // import firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +18,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
